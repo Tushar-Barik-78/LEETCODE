497d36d82a19e9c8b278283831752d69564eb295
Tushar Barik <tusharbarik64@gmail.com>
2026-02-09 23:55:34 +0530

    Time: 3 ms (87.62%), Space: 43.2 MB (84.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+           if(s.length()%2 != 0){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char peekElement = stack.peek();
+                if(ch ==']' && peekElement=='['){
+                    stack.pop();
+                }else if(ch =='}' && peekElement=='{'){
+                    stack.pop();
+                }else if(ch ==')' && peekElement=='('){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            // System.out.println(peekElement);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
