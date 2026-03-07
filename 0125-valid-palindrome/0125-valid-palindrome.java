a913942e03f6a547f5d00110262cc66ebd8907a3
Tushar Barik <tusharbarik64@gmail.com>
2026-03-07 23:33:50 +0530

    Time: 2 ms (99.13%), Space: 44 MB (96.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,47 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        s = s.toLowerCase();
+        // s = s.replaceAll("[^a-zA-Z0-9]","");
+        // s = s.toLowerCase();
+        // int l = 0;
+        // int r = s.length()-1;
+        // while(l<r){
+        //     if(s.charAt(l)!=s.charAt(r)){
+        //         return false;
+        //     }
+        //     l++;
+        //     r--;
+        // }
+
+
+        // return true;
+
+        // // StringBuilder str = new StringBuilder(s);
+        // // return s.equals(str.reverse().toString());
+
         int l = 0;
         int r = s.length()-1;
+
         while(l<r){
-            if(s.charAt(l)!=s.charAt(r)){
+            int lch= s.charAt(l);
+            int rch= s.charAt(r);
+
+            if(!Character.isLetterOrDigit(lch)){
+                l++;
+                continue;
+            }
+             if(!Character.isLetterOrDigit(rch)){
+                r--;
+                continue;
+            }
+            if(Character.toLowerCase(lch) != Character.toLowerCase(rch)){
                 return false;
             }
+
             l++;
             r--;
         }
 
 
         return true;
-
-        // StringBuilder str = new StringBuilder(s);
-        // return s.equals(str.reverse().toString());
     }
 }
